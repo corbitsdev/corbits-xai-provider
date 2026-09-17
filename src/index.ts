c6c3eb89e9305cc4ae516268c373bf85086ebe38
Sawyer <sawyer@dirtroad.dev>
2026-09-17 16:45:00 -0700

    feat: export the OAuth constants from the package root

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-export {
-  XAI_API_KEY_BASE_URL,
-  XAI_DEFAULT_MODELS,
-  XAI_OAUTH_PROXY_BASE_URL,
-  XAI_PROVIDER,
-  XAI_REASONING_EFFORT_OPTION,
-  XAI_REDIRECT_URI,
-  XAI_REFRESH_SKEW_MS,
-  XAI_SESSION_ID_OPTION,
-  XAI_USER_ID_OPTION,
-} from "./constants";
+export * from "./constants";
 
 export {
   xaiOAuthConfig,
